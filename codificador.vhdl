d47d1375ae454586c60fb93d8e3ae138ba0bf33e
Valmir Ferreira da Silva <65004196+valmyr@users.noreply.github.com>
2021-06-03 19:57:34 -0300

    Update codificador.vhdl

--- codificador.vhdl
+++ codificador.vhdl
@@ -2,18 +2,24 @@
 library IEEE;
 IEEE.numeric_bit.all;
 IEEE.std_logic.all;
-entity decodificador is port(input   : in bit_vector(1 downto 0);
-                                en   : in bit;
-                              output : out bit_vector(3 downto 0));
+entity decodificador is port(input     : in std_logic_vector(1 downto 0);
+                              enable   : in std_logic;
+                              output   : out std_logic_vector(3 downto 0));
 end decodificador;
 architecture archDecodificador of decodificador is
-  signal inputConcatenadoComEn : bit_vector(2 downto 0);
   begin
-  inputConcatenadoComEn <= (en & input);
-  with en or input  select
-    output <= bit_vector("0001" when "100",
-                         "0010" when "101",
-                         "0100" when "110",
-                         "1000" when "111",
-                         "0000" when others);
+  --Bloco sequêncial
+  process (enable, input)
+    begin
+      Y <= "ZZZZ";
+      if(enable = '1') then
+          case input is
+            when "00" >= output <= "0001";
+            when "01" >= output <= "0010";
+            when "10" >= output <= "0100";
+            when "11" >= output <= "1000";
+            when others => output <= "ZZZZ";
+          end case;
+      end if;
+  end process;
 end archDecodificador;
