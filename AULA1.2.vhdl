aff5b2538e37b20ec9069da87396ec230c30407d
Valmir Ferreira da Silva <65004196+valmyr@users.noreply.github.com>
2021-06-03 13:18:05 -0300

    Update AULA1.2.vhdl

--- AULA1.2.vhdl
+++ AULA1.2.vhdl
@@ -1,8 +1,8 @@
 --Escreva a entity para o circuito a seguir
 entity entityMySwitch is
-  port(ent1, ent0: in bit_vector(4 downto 0);
-       sel : in  bit_vector(4 downto 0);
-       outp: out bit_vector(4 downto 0));
+  port(ent1, ent0: in bit_vector(3 downto 0);
+       sel : in  bit;
+       outp: out bit_vector(3 downto 0));
 end entityMySwitch;
 architecture archiMySwitch of entityMySwitch is
   begin
