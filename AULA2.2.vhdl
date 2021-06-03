fad359559ea3681338e2e33a81bd40e8f3cea567
Valmir Ferreira da Silva <65004196+valmyr@users.noreply.github.com>
2021-06-03 15:49:47 -0300

    Update AULA2.2.vhdl

--- AULA2.2.vhdl
+++ AULA2.2.vhdl
@@ -5,7 +5,7 @@ entity checkMag is port(input  :in bit_vector(3 downto 0)
                         output : out bit_vector(2 downto 0));
 end checkMag;
 architecture archCheckMag of checkMag is begin
-  with (input < 4 or input < 10 or input < 16) select
+  with (input) select
       output <= "100" when ("0000"|"0001"|"0010"|"0011"),
                 "010" when ("0100"|"0101"|"0110"|"0111"|"1000"|"1001"),
                 "001" when ("1010"|"1011"|"1100"|"1101"|"1110"|"1111"),
