625edd28cdce437ba0b5f5a401b4016e66ad78b9
Valmir Ferreira da Silva <65004196+valmyr@users.noreply.github.com>
2021-06-03 14:07:52 -0300

    Create AULA1.7.vhdl

---
+++ AULA1.7.vhdl
@@ -0,0 +1,8 @@
+entity myFunction is
+  port(L,M,N : in bit;
+           F : out bit);
+  end myFunction;
+architecture archMyFunction of myFunction is
+   begin
+     F <= (not(L) and not(M) and N) or (L and M);
+  end archMyFunction
