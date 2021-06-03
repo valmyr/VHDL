0b54272880e094a111a95adca0ba132163cb1a96
Valmir Ferreira da Silva <65004196+valmyr@users.noreply.github.com>
2021-06-03 13:48:23 -0300

    Update AULA1.5.vhdl

--- AULA1.5.vhdl
+++ AULA1.5.vhdl
@@ -1,12 +1,13 @@
 library IEEE;
 use IEEE.numeric_bit.all; -- Aritimética com / sem sinal
---Somador de 4 bits sem sinal com TS
+--Somador de 4 bits sem sinal com TS sem as variáveis Extensoras
 entity somador4bits is
   port(A , B : in bit_vector(3 downto 0);
        S     : out bit_vector(3 downto 0);
        Ts    : out bit);
 end somador4bits;
 architecture archSomador4bits of somador4bits
+       signal soma_C : bit_vector(4 downto 0);
        begin
          soma_C <= bit_vector(unsigned("0" & A)+unsigned("0" & B);
          S = bit_vector(soma_C(3 downto 0));
