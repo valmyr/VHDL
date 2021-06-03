4e33ee07a8edc2f00bb7133d6407ae7d3db65bed
Valmir Ferreira da Silva <65004196+valmyr@users.noreply.github.com>
2021-06-03 17:46:10 -0300

    Update mux4_1.vhdl

--- mux4_1.vhdl
+++ mux4_1.vhdl
@@ -7,7 +7,7 @@ entity Mux4To1 is port(seletor : in bit_vector (1 downto 0);
 end Mux4To1;
 architecture archMux4To1 of Mux4To1 is begin
     with seletor select
-        ouput <=input(0) when "000",
+        output <=input(0) when "000",
                 input(1) when "001",
                 input(2) when "010",
                 input(3) when "011",
