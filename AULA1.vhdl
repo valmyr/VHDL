3642e93f790b670d691ebf4df9799c4234ecdc1d
Valmir Ferreira da Silva <65004196+valmyr@users.noreply.github.com>
2021-06-03 11:51:53 -0300

    Create AULA1.vhdl

---
+++ AULA1.vhdl
@@ -0,0 +1,6 @@
+entity minha_entidade is 
+    port(signal-nomes: mode signal type;//"signal-name" nome da porta
+    signal-names: mode signal type;//"mode" in,out,inout
+    signal-names: mode signal type);
+end minha_entidade;
+          
