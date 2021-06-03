e2e04f8899c924273b4437d5ce99de835397b92e
Valmir Ferreira da Silva <65004196+valmyr@users.noreply.github.com>
2021-06-03 16:21:28 -0300

    Update AULA2.4.vhdl

--- AULA2.4.vhdl
+++ AULA2.4.vhdl
@@ -4,18 +4,11 @@ entity display7Segments is port(contador   : in bit_vector(1 downto 0);
                                 output : out bit_vector(7 downto 0)--mais significativo = g e menos significativo = a
                                );
 end display7Segments;
-  
 architecture archDisplay7Segments of display7Segments is begin
     with contador select
-        output <= bit_vector("1110011" when '00',
-                              "0000000" when others);
-    with contador select
-        output <= bit_vector("0111111" when '01',
-                              "0000000" when others);
-    with contador select
-        output <= bit_vector("0111000" when '10',
-                             "0000000" when others);
-    with contador select
-        output <= bit_vector("0110000" when '11',
-                              "0000000" when others);       
+        output <= bit_vector("1110011" when '00', --P
+                             "0111111" when '01', --O
+                             "0111000" when '10', --L
+                             "0110000" when '11', --I
+                             "0000000" when others);       
 end archDisplay7Segments;
