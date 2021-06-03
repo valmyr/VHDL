c6f97f9258e0026f488566b3c828b128b9fba016
Valmir Ferreira da Silva <65004196+valmyr@users.noreply.github.com>
2021-06-03 15:49:18 -0300

    Update AULA2.2.vhdl
    método correto!

--- AULA2.2.vhdl
+++ AULA2.2.vhdl
@@ -6,8 +6,8 @@ entity checkMag is port(input  :in bit_vector(3 downto 0)
 end checkMag;
 architecture archCheckMag of checkMag is begin
   with (input < 4 or input < 10 or input < 16) select
-      output <= "100" when (input < 4),
-                "010" when (input < 10),
-                "001" when (input < 16),
+      output <= "100" when ("0000"|"0001"|"0010"|"0011"),
+                "010" when ("0100"|"0101"|"0110"|"0111"|"1000"|"1001"),
+                "001" when ("1010"|"1011"|"1100"|"1101"|"1110"|"1111"),
                 "000" when others;
 end archCheckMag;
