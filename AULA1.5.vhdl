c17eb748646c4eef43b4d0f6125f41592459df18
Valmir Ferreira da Silva <65004196+valmyr@users.noreply.github.com>
2021-06-03 13:52:40 -0300

    Update AULA1.5.vhdl

--- AULA1.5.vhdl
+++ AULA1.5.vhdl
@@ -12,4 +12,4 @@ architecture archSomador4bits of somador4bits is
          soma_C <= bit_vector(unsigned("0" & A)+unsigned("0" & B);
          S <= bit_vector(soma_C(3 downto 0));
          Ts     <=  soma_C(4);
- end archSomador4Bits;
+ end archSomador4bits;
