76bbd12acc2f41ee06b60fad03aa1d02d30f2e6f
Valmir Ferreira da Silva <65004196+valmyr@users.noreply.github.com>
2021-06-04 15:12:39 -0300

    Create AULA4.1.4_PROCESS.vhdl

---
+++ AULA4.1.4_PROCESS.vhdl
@@ -0,0 +1,12 @@
+entity buffer_ is port(input : in bit_vector(7 downto 0);
+                       output: out bit_vector(7 downto 0);
+                       load  : in bit);
+end buffer_;
+architecture  archBuffer_ of bufer_ if begin 
+  processBuffer : process (load) begin
+  --se load "0 -> 1"
+    if(load = '1') then 
+        output <= input;
+    end if;
+  end process processBuffer;
+end archBuffer_;
