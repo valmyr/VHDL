b02abc65491eb5c849181167ced933f38f9f9624
Valmir Ferreira da Silva <65004196+valmyr@users.noreply.github.com>
2021-06-04 15:05:05 -0300

    Create AULA4.1.3_PROCESS.vhdl

---
+++ AULA4.1.3_PROCESS.vhdl
@@ -0,0 +1,15 @@
+--AB'C'+BC
+entity myFunction is port(A,B,C : in bit;
+                              F : out bit);
+end myFunction;
+architecture archMyFunction of myFunction is begin
+  myFunctionLabel : process (A,B,C) begin
+    if(A = '1' and B ='0' and C = '0') then 
+      F <= '1';
+    elsif(B='1' and C='1') then
+      F <= '1'
+    else
+      F<= '0';
+    end if;
+  end process myFunctionLabel;
+end archMyfunction;
