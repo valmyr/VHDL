053b7087295ac7e8e60f44ca2a1fb31710bd4558
Valmir Ferreira da Silva <65004196+valmyr@users.noreply.github.com>
2021-06-04 17:21:17 -0300

    Create flipflopD.vhdl

---
+++ flipflopD.vhdl
@@ -0,0 +1,10 @@
+entity fliFlopD is port(D, clk : in bit;
+                        Q, Qb  : out bit);
+end flipFlopD;
+architecture archFlipFlopD of FlipFlopD is begin 
+  FLIPFLOP_PROCESS : process (clk) begin
+    if(rising_edge(clk)) then 
+      Q <= D;
+      Qb <= not(Q);
+  end process FLIPFLOP_PROCESS;
+end archFlipFlopD;
