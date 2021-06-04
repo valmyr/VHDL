64b862abb5303dd9351fcb007606f23cc04212c2
Valmir Ferreira da Silva <65004196+valmyr@users.noreply.github.com>
2021-06-04 15:13:50 -0300

    Update AULA4.1.4_PROCESS.vhdl

--- AULA4.1.4_PROCESS.vhdl
+++ AULA4.1.4_PROCESS.vhdl
@@ -2,7 +2,7 @@ entity buffer_ is port(input : in bit_vector(7 downto 0);
                        output: out bit_vector(7 downto 0);
                        load  : in bit);
 end buffer_;
-architecture  archBuffer_ of bufer_ if begin 
+architecture  archBuffer_ of bufer_ is begin 
   processBuffer : process (load) begin
   --se load "0 -> 1"
     if(load = '1') then 
